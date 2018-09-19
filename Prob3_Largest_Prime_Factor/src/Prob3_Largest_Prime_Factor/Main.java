1eea0542b3fc5dbfa37127077be3bf49af0cd16d
Anik Paul Gomes <gomesap29@gmail.com>
2018-09-18 22:06:27 -0500

    "Problem 4 solved" & "Timing added to problem 3 "

--- Prob3_Largest_Prime_Factor/src/Prob3_Largest_Prime_Factor/Main.java
+++ Prob3_Largest_Prime_Factor/src/Prob3_Largest_Prime_Factor/Main.java
@@ -32,11 +32,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        long start = System.currentTimeMillis();                                                                        // Records start time
+
+
         Scanner userInput = new Scanner(System.in);
         System.out.print("Enter an integer to find the largest prime factor: " );
         long number = userInput.nextLong();
         List<Long> primeFactorList = primeFactors(number);
         System.out.print("Largest prime factor: " + primeFactorList.get(primeFactorList.size() -1));
+
+
+        System.out.println();
+        long end = System.currentTimeMillis();                                                                          //Records end time
+        System.out.println("This project takes: " + (end - start) + "ms");                                              //Calculates running time
 
 
     }
---
+++ Prob4_Largest_palindrome_product/src/Prob3_Largest_Prime_Factor/Main.java
@@ -0,0 +1,53 @@
+package Prob3_Largest_Prime_Factor;
+
+
+/*
+This is a project euler problem 4
+Largest palindrome product
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        long start = System.currentTimeMillis();
+        System.out.println("The largest palindrome made from the product of two 3-digit numbers: " + largestPalindrome());
+
+        long end = System.currentTimeMillis();
+        System.out.println("This project takes: " + (end - start) + "ms");
+
+    }
+
+    public static long largestPalindrome()
+    {
+        long largestPalin = 0;
+        for (int i = 999; i > 99; i-- )
+        {
+            for(int j = 999; j > 99; j--)
+            {
+                if( (ifPalindrome((i*j)) && ((i*j) > largestPalin)))
+                {
+                    largestPalin = (i*j);
+                }
+
+            }
+        }
+        return largestPalin;
+    }
+
+    public static boolean ifPalindrome(long num)
+    {
+        StringBuilder numString = new StringBuilder();
+        numString.append(num);
+        if( numString.toString().equals((numString.reverse()).toString()))
+        {
+            return true;
+        }
+        return false;
+    }
+}
